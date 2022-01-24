3905126d5f8be529268b3face7c3a45bb1f55475
rachellehcar78 <rachel.phanglyn23@ycdsbk12.ca>
2022-01-24 16:45:00 -0500

    First Changes
    Changes to size, background, started on avatars.

--- Sketch1.java
+++ Sketch1.java
@@ -1,19 +1,49 @@
 import processing.core.PApplet;
 
 public class Sketch1 extends PApplet {
-	
+
+  // Rachel's page
 	
   public void settings() {
-    size(400, 400);
+    size(800, 450);
   }
 
+  
 
   public void setup() {
-    background(210, 255, 173);
+    background(0);
   }
 
   public void draw() {
-	  
+  
+  // head
+  ellipse( width/ 4, height/2, width/16, height/9);
+  fill(255, 200, 122);
+
+  // Charater #1
+  ellipse(200, 200, 50, 50);
+  fill(255);
+
+   // nose
+   ellipse(200, 200, 10, 10);
+   fill(78);
+
+   // mouth
+   strokeWeight((float)width/130);
+   arc(200, 150, 100, 100, 400, 400);
+   
+
+
+  
+
+  // Character #2
+  ellipse(400, 200,  50, 50);
+  fill(255);
+
+  // Character #3
+  ellipse(600, 200, 50, 50);
+  fill(255);
+
 
   }
   
