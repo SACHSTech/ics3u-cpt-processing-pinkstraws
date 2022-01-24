35c19cef1c3ecff15513d5bb003c271e660fe5f8
RomuelC <romuel.cuevas23@ycdsbk12.ca>
2022-01-24 18:09:22 -0500

    Start Page
    Start page almost complete, waiting for avatars to be finished then add to start page to showcase

--- Sketch2.java
+++ Sketch2.java
@@ -42,30 +42,51 @@ public class Sketch2 extends PApplet {
         snowPile[i] += (width / 90);
       }
     }
-    
     // Start Button
     fill(255, 255, 255);
-    rect(250, 100, 300 , 75);
+    rect(225, 100, 300 , 75);
+
+    // How To Play Sign
+    fill(50);
+    textSize(50);
+    text(" How To Play ", 220, 155);
 
     // How To Play Button
     fill(255, 255, 255);
-    rect(300, 200, 200 , 75);
+    rect(275, 200, 200, 75);
+
+    // Play Sign
+    fill(50);
+    textSize(50);
+    text("Play", 330, 255);
 
     // Extreme
     fill(194, 25, 25);
+    rect(10, 100, 80, 30);
+    rect(10, 190, 80, 30);
+    rect(10, 270, 80, 30);
+    rect(10, 100, 30, 200);
 
     // Colour
     fill(18, 179, 21);
-    arc(50, 200, 100, 200, 10, 270);
-    fill(0, 0, 0);
-    arc(70, 230, 100, 200, 10, 270);
+    rect(120, 100, 80, 30);
+    rect(120, 270, 80, 30);
+    rect(120, 100, 30, 200);
 
     // Picker
     fill(24, 15, 209);
+    rect(550, 100, 80, 30);
+    rect(550, 170, 80, 30);
+    rect(620, 100, 30, 100);
+    rect(550, 100, 30, 200);
 
     // Game
     fill(227, 224, 70);
+    rect(680, 100, 90, 30);
+    rect(740, 220, 30, 70);
+    rect(725, 200, 60, 30);
+    rect(680, 270, 90, 30);
+    rect(680, 100, 30, 200);
 
   }
-  
 }
