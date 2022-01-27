724c70524e2ec4d6e4fa1b61032552a162c9fe96
RomuelC <romuel.cuevas23@ycdsbk12.ca>
2022-01-27 16:31:34 -0500

    Added Scoreboard
    Added scoreboard that has a speed bonus when u click faster and +10 points if you get the tile right. Working on a reset function

--- Sketch2.java
+++ Sketch2.java
@@ -26,6 +26,10 @@ public class Sketch2 extends PApplet {
   boolean Level5;
   boolean youwinScreen;
 
+  // Scoreboard
+
+  int points = 0;
+
   public void settings() {
     size(800, 450);
   }
@@ -66,6 +70,9 @@ public class Sketch2 extends PApplet {
     }
     else if(Level3){
       Level3();
+    }
+    else if (youwinScreen){
+      youwinScreen();
     }
     else if(gameoverScreen){
       gameoverScreen();
@@ -293,6 +300,11 @@ public class Sketch2 extends PApplet {
 
   public void Level1(){
     background(0);
+    
+    // Timer
+    fill(255);
+    text(millis(), 500, 30);
+
     // Colour Chosen
     fill(255);
     textSize(70);
@@ -304,6 +316,15 @@ public class Sketch2 extends PApplet {
     if(mouseX > 150 && mouseY > 200 && mouseX < 300 && mouseY < 350 && mousePressed){
       Level2OP = true;
       Level1 = false;
+
+      // Points and Time Bonus
+      /* whenever you click the right box
+      * you get a point. If you also
+      * click the right box fast,
+      * you will get extra points
+      */
+      int timeBonus = ((20000 / millis()) * 10);
+      points = points + 10 + timeBonus;
     }
 
     // Box 2: White
@@ -313,6 +334,8 @@ public class Sketch2 extends PApplet {
       gameoverScreen = true;
       Level1 = false;
     }
+    textSize(20);
+    text("Points: " + points, 10, 30);
   }
 
   public void Level2OP(){  
@@ -331,10 +354,16 @@ public class Sketch2 extends PApplet {
     Level2OP = false;
     Level2 = true;
   }
+  textSize(20);
+  text("Points: " + points, 10, 30);
 }
 
   public void Level2(){
     background(0);
+    // Timer
+    fill(255);
+    text(millis(), 500, 30);
+
     // Colour Chosen
     fill(255, 215, 84);
     textSize(70);
@@ -354,7 +383,11 @@ public class Sketch2 extends PApplet {
     if(mouseX > 500 && mouseY > 200 && mouseX < 650 && mouseY < 350 && mousePressed){
       Level3OP = true;
       Level2 = false;
+      int timeBonus = ((20000 / millis()) * 10);
+      points = points + 10 + timeBonus;
     }
+    textSize(20);
+    text("Points: " + points, 10, 30);
   }
 
   public void Level3OP(){
@@ -373,10 +406,15 @@ public class Sketch2 extends PApplet {
     Level3OP = false;
     Level3 = true;
   }
+  textSize(20);
+  text("Points: " + points, 10, 30);
 }
 
   public void Level3(){
   background(74, 47, 47);
+  // Timer
+  fill(255);
+  text(millis(), 500, 30);
     // Colour Chosen
     fill(53, 2, 54);
     textSize(70);
@@ -394,8 +432,10 @@ public class Sketch2 extends PApplet {
     fill(239, 75, 242);
     rect(325, 200, 150, 150);
     if(mouseX > 325 && mouseY > 200 && mouseX < 475 && mouseY < 350 && mousePressed){
-      Level4OP = true;
+      youwinScreen = true;
       Level3 = false;
+      int timeBonus = ((20000 / millis()) * 10);
+      points = points + 10 + timeBonus;
     }
 
     // Box 3: Purple
@@ -404,6 +444,23 @@ public class Sketch2 extends PApplet {
     if(mouseX > 600 && mouseY > 200 && mouseX < 750 && mouseY < 350 && mousePressed){
       gameoverScreen = true;
       Level3 = false;
+    }
+    textSize(20);
+    text("Points: " + points, 10, 30);
+  }
+
+  public void youwinScreen(){
+    background(0);
+    fill(255);
+    if(points > 100){
+      textSize(50);
+      text("You Win!" , 200, 200);
+      text("Your score was: " + points, 100, 300);
+    }
+    else if(points < 100){
+      textSize(50);
+      text("You Lose!" , 200, 200);
+      text("Your score was: " + points, 100, 300);
     }
   }
 }
