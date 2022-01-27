9faef9cf6933d962cd7e4c649eca07a63cda4884
rachellehcar78 <rachel.phanglyn23@ycdsbk12.ca>
2022-01-27 12:24:55 -0500

    Update
    Added level 4, level 5, small updates to you win screen animations, deleted controls as we changed drags and wheels to clicks.

--- Sketch1.java
+++ Sketch1.java
@@ -7,15 +7,16 @@ public class Sketch1 extends PApplet {
   // Rachel's page
 	
   public boolean mClicked = false; 
-  public boolean mDragged = false;
-  public boolean mWheel = false;
-  public boolean upPressed = false;
-  public boolean leftPressed = false; 
-  public boolean rightPressed = false;
-  public boolean shiftPressed = false;
-
+  boolean minionScreen;
   boolean gameOverScreen;
-  boolean levelTwo;
+  boolean levelFour;
+  boolean levelFiveOP;
+
+  // Variables for minions
+  float bodyX = 400;
+  float bodyY = 150;
+  float bodySize = (193);
+  float bodyDistance = bodySize/2;
 
   public void settings() {
     size(800, 450);
@@ -30,6 +31,10 @@ public class Sketch1 extends PApplet {
   }
 
   public void draw() {
+
+    if(gameOverScreen){
+      gameOverScreen();
+    }
   
   fill(255);
   textSize(30);
@@ -93,23 +98,40 @@ public class Sketch1 extends PApplet {
   textSize(50);
   text("__", 575, 200);
   
-  // Choose your character
-
+  // Enter Game
   if (mClicked) {
     background (0);
-    //  fill(255);
   
-    // Level One
+    // Level Four
     fill(66, 245, 173);
     textSize(40);
     text("Yellow", 350, 50);
     
-    rect(50, 100, 300, 300);
+    rect(50, 100, 200, 200);
+    if(mouseX > 50 && mouseY > 100 && mouseX < 150 && mouseY < 150 && mousePressed){
+      gameOverScreen = true;
+      levelFour = false;
+    }
+   
+    fill(140, 255, 0);
+    rect(300, 100, 200, 200);
+    if(mouseX > 200 && mouseY > 100 && mouseX < 400 && mouseY < 400 && mousePressed){
+      gameOverScreen = true;
+      levelFour = false;
+    }
     
     fill(255, 224, 110);
-    rect(450, 100, 300, 300);
+    rect(550, 100, 200, 200);
+    if(mouseX > 550 && mouseY > 100 && mouseX < 650 && mouseY < 650 && mousePressed){
+      levelFiveOP = true;
+      levelFour = true;
+    }
 
-    // Level Two
+    
+
+    
+
+    /* Level Five
     fill(252, 3, 157);
     textSize(40);
     text("Red", 350, 50);
@@ -118,46 +140,26 @@ public class Sketch1 extends PApplet {
 
     fill(252, 3, 3);
     rect(450, 100, 300, 300);
+    */
 
-    if(mWheel) {
-      background(0);
-      fill(55);
-      textSize(70);
-      text("Level 2!", 275, 225);
-
+ 
       // Add one point to score board
     }
-    if(mDragged){
-        background(0);
-        fill(55);
-        textSize(70);
-        text("Game Over", 400, 400);
-
-        fill(55);
-        textSize(40);
-        text("Press your shift key to restart the game!", 30, 225);
-
-        if(shiftPressed) {
-
-        }
-        
-        // You win screen
-        background(0);
-
-    }
+    
+    
   }
   // You win screen
-  background(0);
-  drawFlower(random(width), random(height), random(20, 55));
-}
+ // background(0);
+//  drawMinionOne(random(width), random(height), random(20, 55));
+//}
   
-void drawFlower(float flowerX, float flowerY, float petalSize) {
-  float petalDistance = petalSize / 2;
+// void drawMinionOne(float flowerX, float flowerY, float petalSize) {
+ //  float petalDistance = petalSize / 2;
 
-  fill(255, 128, 0);
+ // fill(255, 128, 0);
 
   // upper-left petal
-  ellipse(flowerX - petalDistance, flowerY - petalDistance, 
+/*  ellipse(flowerX - petalDistance, flowerY - petalDistance, 
     petalSize, petalSize);
 
   // upper-right petal
@@ -173,44 +175,35 @@ void drawFlower(float flowerX, float flowerY, float petalSize) {
     petalSize, petalSize);
 
   // center petal
-  fill(255, 0, 0);
-  ellipse(flowerX, flowerY, 
-    petalSize, petalSize);
+  fill(49, 181, 222);
+  ellipse(bodyX, bodyY, bodySize, bodySize);
   
   fill(255);
   textSize(100);
   text("You win!", 200, 250);
 }
-
+*/
   // Define other methods
-  
+  public void minionScreen(){
+    background(0);
+    
+  }
+
   public void mouseClicked() {
     mClicked = true;
   }
 
-  public void mouseDragged() {
-    mDragged = true;
-  }
-
-  public void mouseWheel() {
-    mWheel = true;
-  }
-  
-  public void keyPressed(){
-      if(keyCode == LEFT){
-          leftPressed = true;
-      }
-      else if(keyCode == UP){ 
-        upPressed = true;
-      }
-      else if(keyCode == RIGHT) {
-        rightPressed = true;
-      }
-
-      if(keyCode == SHIFT){
-
-        
-      }
-
+  public void gameOverScreen(){
+    background(0);
+    fill(55);
+    textSize(70);
+    text("Game Over", 400, 400);
+    textSize(30);
+    text("Press 'm' to go back to menu", 200, 400);
+    if(key == 'm') {
+      gameOverScreen = false;
     }
-  }
+  } 
+
+
+}
