9f2c55afa9e70259e315f4309bc19d781095a72a
rachellehcar78 <rachel.phanglyn23@ycdsbk12.ca>
2022-01-28 14:06:12 -0500

    Organizing
    Formatting and organizing for filming

--- Sketch1.java
+++ Sketch1.java
@@ -1,10 +1,11 @@
 import processing.core.PApplet;
 
-public class Sketch1 extends PApplet {
+public class Sketch1 extends PApplet 
+{
 
   // Rachel's page
 
-  // Frame booleans
+  // Booleans for different frames
     boolean minionScreen;
     boolean gameOverScreen;
     boolean levelFourOP;
@@ -20,20 +21,21 @@ public class Sketch1 extends PApplet {
     float bodyDistance = bodySize/2;
 
   // Array Scoreboard
-  int points = 0;
+    int points = 0;
 
-
-  public void settings() 
+public void settings() 
   {
     size(800, 450);
   }
   
-  public void setup() 
+public void setup() 
   {
     minionScreen = true;
   }
 
-  public void draw() 
+public void draw() 
+  // Order of Screens
+
   {
     if(minionScreen)
     {
@@ -65,9 +67,8 @@ public class Sketch1 extends PApplet {
     }
   }
    
-  public void minionScreen()
+public void minionScreen()
   {
-  
     background(0);
     fill(255);
     textSize(30);
@@ -95,6 +96,7 @@ public class Sketch1 extends PApplet {
     textSize(75);
     text("~", 180, 230);
 
+
   // Character #2
     fill(255, 143, 205);
     ellipse(width/2, height/3,  width/5, height/2);
@@ -112,6 +114,7 @@ public class Sketch1 extends PApplet {
     fill(255, 42, 38);
     textSize(50);
     text("O", 400, 230);
+
 
   // Character #3
     fill(183, 74, 255);
@@ -131,15 +134,15 @@ public class Sketch1 extends PApplet {
     textSize(50);
     text("__", 575, 200);
 
-   // Enter Game
+  // Enter Game
    if (mouseX < width && mouseY < height && mousePressed)
     {
       levelFourOP = true;
       minionScreen = false;
     }
-   }
+  }
   
-  public void levelFourOP()
+public void levelFourOP()
   {
     background(255);
     fill(0);
@@ -150,7 +153,7 @@ public class Sketch1 extends PApplet {
     textSize(45);
     text("Click the 'o' key when you are ready!", 25, 400);
 
-    
+  // 'o' key is pressed then level four screen is called
     if(key == 'o')
     {
       levelFour = true;
@@ -158,11 +161,11 @@ public class Sketch1 extends PApplet {
     }
   }
   
-  public void levelFour()
+public void levelFour()
   {
     background(0);
 
-    // Timer
+  // Timer
     fill(255);
     text(millis(), 740, 30);
 
@@ -170,23 +173,22 @@ public class Sketch1 extends PApplet {
     textSize(40);
     text("Yellow", 345, 50);
     
-  // box 1
+  // Box 1
     rect(50, 100, 200, 200);
     if(mouseX > 50 && mouseY > 100 && mouseX <250 && mouseY < 300 && mousePressed)
       { 
         gameOverScreen = true;
         levelFour = false;
       }
-  // box 2
+  // Box 2
     fill(140, 255, 0);
     rect(300, 100, 200, 200);
     if(mouseX > 300 && mouseY > 100 && mouseX < 500 && mouseY < 300 && mousePressed)
       {
         gameOverScreen = true;
         levelFour = false;
-
       }
-  // correct box 3
+  // Box 3 (Correct Answer)
     fill(255, 224, 110);
     rect(550, 100, 200, 200);
     if(mouseX > 550 && mouseY > 100 && mouseX < 750 && mouseY < 300 && mousePressed)
@@ -194,6 +196,7 @@ public class Sketch1 extends PApplet {
         levelFiveOP = true;
         levelFour = false;
 
+        // Calculates + stores points
         int timeBonus = ((20000 / millis())*10);
         points = points +10 + timeBonus;
       }
@@ -201,7 +204,7 @@ public class Sketch1 extends PApplet {
       text("Points: " + points, 10, 30);
     }
 
-  public void levelFiveOP()
+public void levelFiveOP()
   {
     background(255);
     fill(0);
@@ -212,19 +215,21 @@ public class Sketch1 extends PApplet {
     textSize(35);
     text("Click the 'q' key when you are ready!", 25, 400);
     
+  // If 'q' key is pressed, level Five Screen is true and OP is false
     if(key == 'q')
     {
       levelFive = true;
       levelFiveOP = false;
     }
     textSize(20);
-      text("Points: " + points, 10, 30);
+    text("Points: " + points, 10, 30);
   }
 
-  public void levelFive()
+public void levelFive()
   {
     background(0);
-    // Timer
+
+  // Timer
     fill(255);
     text(millis(), 740, 30);
 
@@ -232,7 +237,7 @@ public class Sketch1 extends PApplet {
     textSize(40);
     text("Red", 350, 50);
 
-    // box 1
+  // Box 1
     rect(50, 100, 300, 100);
     if(mouseX > 50  && mouseY > 100 && mouseX < 350 && mouseY < 200 && mousePressed)
     {
@@ -240,7 +245,7 @@ public class Sketch1 extends PApplet {
       levelFive = false;
     }
 
-    // Correct Rectangle box 2
+  // Box 2 (Correct Answer)
     fill(252, 3, 3);
     rect(450, 100, 300, 100);
     if(mouseX > 450  && mouseY > 100 && mouseX < 750 && mouseY < 200 && mousePressed)
@@ -248,10 +253,11 @@ public class Sketch1 extends PApplet {
       winScreen = true;
       levelFive = false;
 
+      // Calculates + stores points
       int timeBonus = ((20000/ millis())*10);
       points = points + 10 + timeBonus;
     }
-    // box 3
+  // Box 3
     fill(255, 115, 0);
     rect(50, 225, 200, 100);
     if(mouseX > 50  && mouseY > 225 && mouseX < 250 && mouseY < 325 && mousePressed)
@@ -259,7 +265,7 @@ public class Sketch1 extends PApplet {
       gameOverScreen = true;
       levelFive = false;
     }
-    // box 4
+  // Box 4
     fill(247, 0, 255);
     rect(300, 225, 200, 100);
     if(mouseX > 300  && mouseY > 225 && mouseX < 500 && mouseY < 325 && mousePressed)
@@ -267,7 +273,7 @@ public class Sketch1 extends PApplet {
       gameOverScreen = true;
       levelFive = false;
     }
-    // box 5
+  // Box 5
     fill(111, 24, 242);
     rect(550, 225, 200, 100);
     if(mouseX > 550  && mouseY > 225 && mouseX < 750 && mouseY < 325 && mousePressed)
@@ -279,7 +285,7 @@ public class Sketch1 extends PApplet {
     text("Point: "+points, 10, 30);
   }
 
-  public void gameOverScreen()
+public void gameOverScreen()
   {
     background(0);
     fill(55);
@@ -287,17 +293,21 @@ public class Sketch1 extends PApplet {
     text("Game Over", 200, 200);
     textSize(30);
     text("Press 'm' to go back to menu", 200, 400);
+
+  // If 'm' key is pressed, then the code will go back to the minionScreen 
     if(key == 'm') 
     {
       minionScreen();
     }
   } 
 
-  public void winScreen()
+public void winScreen()
   {
     background(255, 161, 133);
-    // Background Design
+    
     stroke(255);
+
+  // For loop for line design
     for(int i = 1; i<= 9; i++)
     {
       strokeWeight(i);
@@ -305,6 +315,7 @@ public class Sketch1 extends PApplet {
       int lineX = i * 30;
       line(lineX, 0, lineX, height);
     }
+  // Randomizes minions size, placement
     drawMinionOne(random(width), random(height), random(10, 50));
   }
   
@@ -337,6 +348,7 @@ public class Sketch1 extends PApplet {
     textSize(100);
     text("You win!", 200, 250);
 
+    // Play Again Button
     fill(102);
     rect(75, 310, 250, 50);
     if(mouseX > 75  && mouseY > 310 && mouseX < 325 && mouseY < 360 && mousePressed)
@@ -349,6 +361,7 @@ public class Sketch1 extends PApplet {
     textSize(40);
     text("Play Again!", 100, 350);
 
+  // Return to Menu Button
     fill(102);
     rect(375, 310, 350, 50);
     if(mouseX > 375  && mouseY > 310 && mouseX < 725 && mouseY < 425 && mousePressed)
@@ -356,13 +369,9 @@ public class Sketch1 extends PApplet {
       minionScreen = true;
       winScreen = false;
     }
-
     fill(255);
     textSize(40);
     text("Return to Menu", 410, 350);
-
-    
   }
-  
 }
 
